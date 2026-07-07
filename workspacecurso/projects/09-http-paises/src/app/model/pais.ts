f72dbdf6c46db985300cbe8414aeefba8a1af72d
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-07-07 16:42:05 +0200

    Adding 09-http-paises project. Still not running.

---
+++ workspacecurso/projects/09-http-paises/src/app/model/pais.ts
@@ -0,0 +1,13 @@
+export class Pais{
+  nombre:string
+  continente:string
+  poblacion:number
+  bandera:string
+  constructor(nombre:string,continente:string,poblacion:number,bandera:string){
+    this.nombre=nombre;
+    this.continente=continente;
+    this.poblacion=poblacion;
+    this.bandera=bandera;
+  }
+
+}
---
+++ workspacecurso/projects/09-http-paises/src/app/service/paises-service.ts
@@ -0,0 +1,10 @@
+import { Service } from '@angular/core';
+import { Pais } from '../model/pais';
+
+@Service()
+export class PaisesService {
+  listaPaises=Pais[]=[];
+
+
+
+}
