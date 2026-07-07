4b33c202761edbcdc8b3d76f4a14010f6b030fb0
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-07-07 16:37:25 +0200

    Project 07_buscador working fine.

--- workspacecurso/projects/07_buscador/src/app/controller/buscador/buscador.ts
+++ workspacecurso/projects/07_buscador/src/app/controller/buscador/buscador.ts
@@ -1,30 +1,34 @@
+import { CommonModule, DatePipe, UpperCasePipe } from '@angular/common';
+import { Resultado } from '../../model/resultado';
 import { BuscadorService } from './../../service/buscador-service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-buscador',
-  imports: [],
+  imports: [DatePipe,UpperCasePipe,FormsModule,CommonModule],
   templateUrl: './buscador.html',
   styleUrl: './buscador.css',
 })
 export class Buscador implements OnInit{
   tematicas:string[]=[];
   tematica:string="--Temática--";
-  resultados:Resultado[];
+  resultados:Resultado[]=[];
 
 
   constructor(private buscadorService:BuscadorService){
-    this.buscadorService.obtenerTematicas()
-      .subscribe(tematicas=>this.tematicas=tematicas);
-  }
-  mostrarTematica(tematica:string):void{
-    this.buscadorService.porTematica(tematica)
-      .subscribe(resultados=>this.resultados = resultados);
   }
   ngOnInit():void{
     this.buscadorService.obtenerTematicas()
       .subscribe(data=>this.tematicas=data);
     this.resultados=[];
+  }
+  mostrarTematica():void{
+    this.buscadorService.porTematica(this.tematica)
+      .subscribe({
+        next:resultados=>this.resultados = resultados,
+        error:err=>alert(err)
+      })
   }
 
 }
