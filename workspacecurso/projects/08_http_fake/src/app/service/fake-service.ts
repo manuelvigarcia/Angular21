bc7725d7ec3cd6b4b29ab8cc0362776d8c0d4981
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-07-07 16:39:02 +0200

    Project 08_http_fake adding more methods using Observable and Observable manipulation.

--- workspacecurso/projects/08_http_fake/src/app/service/fake-service.ts
+++ workspacecurso/projects/08_http_fake/src/app/service/fake-service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, Service } from '@angular/core';
 import { Post } from '../model/Post';
 import { Observable } from 'rxjs'
+import { filter } from 'rxjs'
+
 
 @Injectable({
   providedIn: 'root',
@@ -18,4 +20,17 @@ export class FakeService {
   obtenerComentariosPost(postId:number):Observable<Comentario[]>{
     return this.http.get<Comentario[]>(this.urlBase+"comments",{params:{postId:postId}});
   }
+  obtenerTitulosPost():Observable<string[]>{
+    return this.obtenerPosts()
+              .pipe(map(a=>a.map(n=>n.title)));
+  }
+
+  obtenerPostUsuario(userId:number):Observable<Post[]>{
+    /*return this.obtenerPosts()
+               .pipe(filter(n=>n.userId==userId));
+    Esto no funciona porque lo que viene en el Observable de ObtenerPosts() es toda la lista, no un stream/flujo*/
+    return this.obtenerPosts()
+              .pipe(map(a=>a.filter(n=>n.userId==userId)));
+  }
+
 }
