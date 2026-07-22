10b09b30755bf978bf1bf0aca791bb9bebfa786d
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-07-22 14:17:19 +0200

    (WORKING) Project 14, Table display using event to select argument and Material Design for aesthetics is working correctly.

--- workspacecurso/projects/14_paises_material/src/app/controller/paises/paises.ts
+++ workspacecurso/projects/14_paises_material/src/app/controller/paises/paises.ts
@@ -1,48 +1,52 @@
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit, signal, ViewChild } from '@angular/core';
 import { PaisesService } from './../../service/paises-service';
-import { FormsModule } from '@angular/forms';
-import { Component, signal, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Pais } from '../../model/pais';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortHeader, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-paises',
-  imports: [CommonModule,MatTableModule,
+  imports: [MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     MatSortHeader,
-    FormsModule],
+    FormsModule,CommonModule,
+    MatButtonModule,
+    MatInputModule],
   templateUrl: './paises.html',
   styleUrl: './paises.css',
 })
-export class Paises implements OnInit, AfterViewInit{
+export class Paises implements OnInit,AfterViewInit{
   listapaises=signal<Pais[]>([]);
+  //selectedcontinent=signal<string>("-Continente")
   listacontinentes=signal<string[]>([])
-  selectedcontinent=signal<string>("")
   displayedColumns: string[] = ['Pais', 'Continente', 'Poblacion','Bandera'];
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator)
+  set matPaginator(paginator: MatPaginator) {
+    if (paginator) {
+      this.paginator = paginator;
+      this.dataSource.paginator = paginator;
+    }
+  }paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   dataSource:MatTableDataSource<Pais> = new MatTableDataSource<Pais>();
-
   constructor(private paisesService:PaisesService){}
   ngAfterViewInit(): void {
     this.dataSource.paginator=this.paginator;
-    this.dataSource.sort=this.sort;  }
+    this.dataSource.sort=this.sort;
+  }
   ngOnInit(): void {
-    this.paisesService.obtenerContinentes()
-        .subscribe(continentes=>this.listacontinentes.set(continentes));
+    this.paisesService.obtenerContinentes().subscribe(continentes=>this.listacontinentes.set(continentes));
   }
-  paisesDeContinente():void{
-    this.paisesService.obtenerPaises(this.selectedcontinent())
-      .subscribe(data=>this.listapaises.set(data));
-      this.obtenerContinentes();
-      this.dataSource.data=this.listapaises();
+  cargarPaises(event:any):void{
+    this.paisesService.obtenerPaises(event.target.value).subscribe(data=>{
+                          this.listapaises.set(data);
+                          this.dataSource.data=this.listapaises();
+                      });
   }
-  obtenerContinentes():void{
-    this.paisesService.obtenerContinentes()
-        .subscribe(data=>this.listacontinentes.set(data))
-  }
-
 }
--- workspacecurso/projects/14_paises_material/src/app/service/paises-service.ts
+++ workspacecurso/projects/14_paises_material/src/app/service/paises-service.ts
@@ -4,29 +4,29 @@ import { Observable, of, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PaisesService {
-  paises:Pais[]=[];
-  continentes:string[]=[]
+
   url:string="https://countries.dev/countries?fields=name,region,population,flags"
 
   constructor(private http:HttpClient){}
 
-  private loadPaises():Observable<Pais[]>{
-    return this.http.get<Pais[]>(this.url)
+  getPaises():Observable<Pais[]>{
+    return this.http.get<Pais[]>(this.url);
   }
 
   obtenerContinentes():Observable<string[]>{
-    this.loadPaises().subscribe(paises=>{
-                          this.paises=paises;
-                          this.continentes=Array.from(new Set(this.paises.map(p=>p.region)));
-                        });
-    return this.loadPaises().pipe(
-          map(arrayPaises=>[...new Set(arrayPaises.map(pais=>pais.region))].sort()));
+    return this.getPaises()
+                .pipe(
+                     map(ar=>[...new Set(ar.map(a=>a.region))])
+                );
   }
 
   obtenerPaises(continente:string):Observable<Pais[]>{
-    return of(this.paises.filter(p=>p.region==continente));
+    return this.getPaises()
+                .pipe(
+                    map(ar=>ar.filter(e=>e.region==continente))
+                );
   }
 }
